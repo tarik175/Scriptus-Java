b742f3138d80c19b0ac60b75012c1b84c03354b0
Tarık <ahmettarikzengin2002@gmail.com>
2023-01-13 23:45:02 +0300

    addition

---
+++ Day02_NumbersAndConcat.js
@@ -0,0 +1,34 @@
+let myFavNum=15.4567
+
+let ceiledNum=Math.ceil(myFavNum);
+console.log(ceiledNum)//16
+
+let flooredNum=Math.floor(myFavNum);
+console.log(flooredNum)//15
+
+let roundedNum=Math.round(myFavNum);
+console.log(roundedNum)//15
+
+let fixedNum=myFavNum.toFixed(2);
+console.log(fixedNum)//15.46
+
+let fName=`Jonny`
+let lName=`Cash`
+
+let str1=`My name is `+fName
+console.log(str1)
+
+let str2="My name is `Jonny`"
+console.log(str2)
+
+////////////////////////////////////////////////
+
+let firstName=`Sam`
+let lastName=`Walton`
+let age=50
+let location=`Dallas`
+
+let info=`First name: ${firstName}, Last name: ${lastName}`
+console.log(info)
+
+
