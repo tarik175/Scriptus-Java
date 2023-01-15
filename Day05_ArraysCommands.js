dded74fff9e9d80be132a11d3629cc8859481f1e
Tarık <ahmettarikzengin2002@gmail.com>
2023-01-15 23:14:41 +0300

    arrays

---
+++ Day05_ArraysCommands.js
@@ -0,0 +1,25 @@
+
+console.clear()
+var myFriendList=new Array("sam","tom","tim","john","bob")
+console.log(myFriendList)
+
+
+myFriendList.push("jack")
+console.log(myFriendList)
+
+
+myFriendList.push(true)
+console.log(myFriendList)
+
+
+myFriendList.unshift("fred")
+console.log(myFriendList)
+
+myFriendList.pop()
+console.log(myFriendList)
+
+myFriendList.shift()
+console.log(myFriendList)
+
+
+
--- Day05_ArraysIntro.js
+++ Day05_ArraysIntro.js
@@ -3,10 +3,16 @@
 console.clear();
 
 var friendList= ["sam","tom","tim","jon","bob"]
-console.log(FriendList)
+console.log(friendList)
 
 
 var myFriendList=new Array("sam","tom","tim","john","bob")
 console.log(myFriendList)
 
 
+friendList[5]="alex"
+console.log(friendList)
+
+friendList[7]="raul"
+console.log(friendList)
+console.log(typeof(friendList))
