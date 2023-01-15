6c289c5fec63d363c46ccaa3d1d16c5b0b3b2045
Tarık <ahmettarikzengin2002@gmail.com>
2023-01-15 23:21:30 +0300

    destructring

---
+++ Day06_Destructring.js
@@ -0,0 +1,22 @@
+
+const x=[1,2,3,4,5]
+console.log(x[0])
+
+
+
+
+const [y, z]=x;
+console.log(y)
+console.log(z)
+
+
+const user={
+    id:42,
+    isVerified:true
+}
+console.log(user.id)
+
+
+const {id, isVerified}=user;
+console.log(id)
+console.log(isVerified)
